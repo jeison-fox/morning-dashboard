1b7c80075f3207ef629b87b74c55296a50c54bcf
Jeison Fox <jeisonfox@gmail.com>
2025-06-10 00:48:23 -0500

    Feature: Adding debug console errors for API KEY.

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -6,8 +6,8 @@ export function GET(request: NextRequest) {
   const apiKey = request.headers.get("x-api-key");
   const serverApiKey = process.env.API_KEY;
 
-  console.log("Received API Key from header:", apiKey ? `"...${apiKey.slice(-4)}"` : "null or undefined");
-  console.log("Server API Key from env:", serverApiKey ? `"...${serverApiKey.slice(-4)}"` : "null or undefined");
+  console.error("GEO_API_DEBUG: Received API Key from header:", apiKey ? `"...${apiKey.slice(-4)}"` : "null or undefined");
+  console.error("GEO_API_DEBUG: Server API Key from env:", serverApiKey ? `"...${serverApiKey.slice(-4)}"` : "null or undefined");
 
   if (apiKey !== serverApiKey) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
